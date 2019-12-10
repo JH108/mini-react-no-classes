20013605aff9463f9bc10413cebe7286854aa0a1
Jesse Hill <jsnahill@gmail.com>
2019-12-10 17:04:43 -0600

    Move Library into separate folder
    The end goal is to have a CRUD app built on the library so I will build the app in the src folder and the library in lib.

--- src/index.js
+++ src/index.js
@@ -1,66 +1,15 @@
+import R from "../lib";
 import "./styles.css";
 
 const app = document.getElementById("app");
 
-const MyLibrary = {
-  createTextElement(text) {
-    return {
-      type: "TEXT_ELEMENT",
-      props: {
-        nodeValue: text,
-        children: []
-      }
-    };
-  },
-  // The createElement Function
-  createElement(type, props, ...children) {
-    return {
-      type,
-      props: {
-        ...props,
-        children: children.map(child =>
-          typeof child === "object" ? child : this.createTextElement(child)
-        )
-      }
-    };
-  },
-  // The render Function
-  render(element, container) {
-    const dom =
-      element.type === "TEXT_ELEMENT"
-        ? document.createTextNode("")
-        : document.createElement(element.type);
-
-    const isProperty = key => key !== "children";
-    const addAttribute = name => {
-      dom[name] = element.props[name];
-    };
-
-    Object.keys(element.props)
-      .filter(isProperty)
-      .forEach(addAttribute);
-
-    element.props.children.forEach(child => {
-      this.render(child, dom);
-    });
-
-    container.appendChild(dom);
-  }
-  // Concurrent Mode
-  // Fibers
-  // Render and Commit Phases
-  // Reconciliation
-  // Function Components
-  // Hooks
-};
-
-/** @jsx MyLibrary.createElement */
-const element = MyLibrary.createElement(
+/** @jsx R.createElement */
+const element = R.createElement(
   "div",
   { id: "first", title: "Ha" },
-  MyLibrary.createElement("a", null, "it worked!"),
-  MyLibrary.createElement("b"),
-  MyLibrary.createElement("h3", null, "First h3 with my library")
+  R.createElement("a", null, "it worked!"),
+  R.createElement("b"),
+  R.createElement("h3", null, "First h3 with my library")
 );
 
-MyLibrary.render(element, app);
+R.render(element, app);
